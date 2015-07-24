14f4039b4f9fb4bc15938740d0c80e3c7596ef0a
Nick Z <Nick@Nicks-Mac.local>
2015-07-24 16:59:34 +0800

    Initial Commit

---
+++ LeftSlideViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeftSlideViewController
+//
+//  Created by Nick Z on 15/7/24.
+//  Copyright (c) 2015年 Nick Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
